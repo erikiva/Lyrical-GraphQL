3cb1173ee4dc098d29204bcf254bfcadbb8734b9
natalia <nataliav@gmail.com>
2017-05-01 23:32:01 +0200

    cleated initial component and setup

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://natalia:lyr1c4ldb@ds127731.mlab.com:27731/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
