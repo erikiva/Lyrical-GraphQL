5a67b11e8f9dcab5b84b753c0c57426477a66391
natalia <nataliav@gmail.com>
2017-05-02 00:46:43 +0200

    Added SongList component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
 class SongList extends Component {
+  renderSongs() {
+    return this.props.data.songs.map(song => 
+      <li key={song.id} className="collection-item">
+        {song.title}
+      </li>);
+  }
   render() {
+    if (this.props.data.loading ){
+      return (
+        <div>
+         Loading... 
+        </div>
+      )
+    }
     return (
-      <div>
-        SongList
-      </div>
+      <ul className="collection">
+        {this.renderSongs()}
+      </ul>
     )
   }
 }
 
-export default SongList;
+// definition of the query
+const query = gql`
+  {
+    songs {
+      id,
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
